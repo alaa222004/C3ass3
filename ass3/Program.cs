a3516acdfee118fffe87e6a00b19f25f26a94510
alaa samer <alaasamer222004@gmail.com>
2025-06-25 22:13:33 +0300

    Q2 solved

--- ass3/Program.cs
+++ ass3/Program.cs
@@ -25,6 +25,22 @@ namespace ass3
             }
 
             #endregion
+
+            #region Q2
+            //2 - Write a program that allows the user to insert an integer then print negative
+            //    if it is negative number otherwise print positive.
+
+            int num;
+            num = int.Parse(Console.ReadLine() !);
+            if (num<0)
+            {
+                Console.WriteLine("negtive");
+            }
+            else
+            {
+                Console.WriteLine("postive");
+            }
+            #endregion
         }
     }
 }
