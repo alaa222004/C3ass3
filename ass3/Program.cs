3e4fa476db75a194e9074ff60c14897bc52599e4
alaa samer <alaasamer222004@gmail.com>
2025-06-25 22:09:33 +0300

    Q1 solved

--- ass3/Program.cs
+++ ass3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace ass3
 {
@@ -6,7 +7,24 @@ namespace ass3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            #region Q1
+            //1 - Write a program that takes a number from the user then print yes if
+            //    that number can be divided by 3 and 4 otherwise print no.
+
+            int number;
+            number=int.Parse(Console.ReadLine()!);
+
+            if(number%3==0 &&  number%4==0) 
+                {
+                Console.WriteLine("yes");
+                }
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+            #endregion
         }
     }
 }
