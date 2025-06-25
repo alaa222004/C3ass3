a1a4b79c7b6adba1f285e8f1183685b097400320
alaa samer <alaasamer222004@gmail.com>
2025-06-25 22:40:08 +0300

    Q7 solved

--- ass3/Program.cs
+++ ass3/Program.cs
@@ -116,6 +116,18 @@ namespace ass3
                 Console.WriteLine(i);
             }
             #endregion
+
+            #region Q7
+//            7 - Write a program that allows the user to insert an integer then
+//             print a multiplication table up to 12.
+
+            int mult=int.Parse(Console.ReadLine() !);
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine($"{i} * {mult} = {i * mult}");
+            }
+
+            #endregion
         }
     }
 }
